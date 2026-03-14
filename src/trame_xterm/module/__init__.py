3e1addaac5632144ea0b433c10d7f236fdfa31fd
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-03-13 18:54:19 -0600

    fix(js): add version in url

--- src/trame_xterm/module/__init__.py
+++ src/trame_xterm/module/__init__.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+from trame_xterm import __version__
+
 serve_path = str(Path(__file__).with_name("serve").resolve())
-serve = {"__trame_xterm": serve_path}
-scripts = ["__trame_xterm/trame-xterm.umd.js"]
-styles = ["__trame_xterm/style.css"]
+serve = {f"__trame_xterm_{__version__}": serve_path}
+scripts = [f"__trame_xterm_{__version__}/trame-xterm.umd.js"]
+styles = [f"__trame_xterm_{__version__}/style.css"]
 vue_use = ["trame_xterm"]
